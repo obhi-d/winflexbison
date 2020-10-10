e216a25d65b4f9cdb38abefad98458d4f3cc742d
obhi <obi.de.online@gmail.com>
2020-10-10 22:25:43 +0200

    Fix VS2019 build w.r.t. corecrt_io.h/dup_str() redefinition

--- common/m4/output.c
+++ common/m4/output.c
@@ -17,7 +17,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#include <corecrt_io.h>
 #include "m4.h"
 
 #include <limits.h>
